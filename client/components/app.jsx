17e4c0b5f2683ea9d7e2c37a93053abc9a04091b
Julian <jthrow00@gmail.com>
2020-03-09 11:18:56 -0700

    Modified App class component to be blank with only constructor with empty obj state

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,23 +3,10 @@ import React from 'react';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      message: null,
-      isLoading: true
-    };
-  }
-
-  componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
+    this.state = {};
   }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{ this.state.message }</h1>;
+    return null;
   }
 }
