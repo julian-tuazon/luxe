f2e1fbf08590491b6c180a977c10b7a5cfef6105
Julian <jthrow00@gmail.com>
2020-03-09 17:37:45 -0700

    Added margin, padding to Header component

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Header(props) {
-  return <h3 className="text-white bg-dark"><i className="fas fa-dollar-sign"></i>Wicked Sales</h3>;
+  return <h5 className="text-white bg-dark p-3 mb-5"><i className="fas fa-dollar-sign fa-sm mr-3"></i>Wicked Sales</h5>;
 }
