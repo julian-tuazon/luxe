0300d4e83e4b15622a0088e2d2c48d134d4f9ebf
Julian <jthrow00@gmail.com>
2020-03-09 17:33:36 -0700

    Added bootstrap styling classes for back to catalog link - btn, flex, padding

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -5,6 +5,7 @@ export default class ProductDetails extends React.Component {
     super(props);
     this.state = { product: null };
     this.getProductData = this.getProductData.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -18,19 +19,24 @@ export default class ProductDetails extends React.Component {
       .catch(err => console.error(err));
   }
 
+  handleClick() {
+    this.props.setView('catalog', {});
+    this.setState({ product: null });
+  }
+
   render() {
     if (!this.state.product) return <h1>Loading...</h1>;
     return (
       <div className="row mt-5">
         <div className="card col-7 mx-auto">
-          <div className="text-muted my-3">Back to catalog</div>
+          <div className="text-muted my-3 px-0 btn d-flex justify-content-start" onClick={this.handleClick}>Back to catalog</div>
           <div className="row">
             <img
               src={this.state.product.image}
               className="card-img-top col-4"
               alt={this.state.product.name}
-              style={{ objectFit: 'contain', height: '50%' }} />
-            <div className="d-flex flex-column">
+              style={{ objectFit: 'contain', maxHeight: '300px' }} />
+            <div className="d-flex flex-column col-7">
               <h3 className="card-title">{this.state.product.name}</h3>
               <h5 className="text-muted">${this.state.product.price}</h5>
               <p>{this.state.product.shortDescription}</p>
