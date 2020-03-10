4d8ae1d9d304849741d2e645eb66e915958fb6f2
Julian <jthrow00@gmail.com>
2020-03-10 13:23:29 -0700

    Modified Header renderr to include flex container; altered width, margin, padding

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 export default function Header(props) {
   return (
-    <div className="d-flex justify-content-between">
-      <h5 className="text-white bg-dark p-3 mb-5">
-        <i className="fas fa-dollar-sign fa-sm mr-3"></i>
-        Wicked Sales
-      </h5>
-      <h5 className="text-white bg-dark p-3 mb-5">
-        <i className="fas fa-shopping-cart fa-sm mr-3"></i>
-        {props.cartItemCount}
-      </h5>
+    <div className="py-3 mx-0 mb-5 row text-white bg-dark">
+      <div className="col-11 mx-auto d-flex justify-content-between">
+        <h5>
+          <i className="fas fa-dollar-sign fa-sm mr-3"></i>
+          Wicked Sales
+        </h5>
+        <h5>
+          <i className="fas fa-shopping-cart fa-sm mr-3"></i>
+          {props.cartItemCount}
+        </h5>
+      </div>
     </div>
-
   );
 }
