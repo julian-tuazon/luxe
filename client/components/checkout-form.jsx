4828c97168775d72b4fb209df27ae2635210ce62
Julian <jthrow00@gmail.com>
2020-03-11 00:52:58 -0700

    Placed input elements in CheckoutForm in form element

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -32,12 +32,14 @@ export default class CheckoutForm extends React.Component {
         <div className="col-7 mx-auto d-flex flex-column">
           <h2 className="mb-4">My Cart</h2>
           <h5 className="d-flex align-items-center text-muted mb-4">Total Price: ${this.getTotalPrice()}</h5>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" className="mb-4" onChange={this.handleChange} />
-          <label htmlFor="card">Credit Card</label>
-          <input type="text" id="card" className="mb-4" onChange={this.handleChange} />
-          <label htmlFor="name">Shipping Address</label>
-          <textarea type="textarea" id="address" className="mb-4" rows="4" onChange={this.handleChange} />
+          <form className="d-flex flex-column">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" className="mb-4" onChange={this.handleChange} />
+            <label htmlFor="card">Credit Card</label>
+            <input type="text" id="card" className="mb-4" onChange={this.handleChange} />
+            <label htmlFor="name">Shipping Address</label>
+            <textarea type="textarea" id="address" className="mb-4" rows="4" onChange={this.handleChange} />
+          </form>
           <div className="d-flex justify-content-between">
             <div className="text-muted mb-4 pt-0 px-0 btn d-flex justify-content-start" id="catalog" onClick={this.handleClick}>
               Back to catalog
