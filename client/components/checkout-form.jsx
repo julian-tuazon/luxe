d7deaa17282f2ea769fea79513ce1fcaf53e3b55
Julian <jthrow00@gmail.com>
2020-03-11 00:26:53 -0700

    Modified CheckoutForm render to have input/labels with no div container and readded margin bottom; bound handleClick to this

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default class CheckoutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
   getTotalPrice() {
     return this.props.cart.reduce((acc, cur) => acc + cur.price, 0);
@@ -16,18 +20,12 @@ export default class CheckoutForm extends React.Component {
         <div className="col-7 mx-auto d-flex flex-column">
           <h2 className="mb-4">My Cart</h2>
           <h5 className="d-flex align-items-center text-muted mb-4">Total Price: ${this.getTotalPrice()}</h5>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" />
-          </div>
-          <div>
-            <label htmlFor="card">Credit Card</label>
-            <input type="text" id="card" />
-          </div>
-          <div>
-            <label htmlFor="name">Shipping Address</label>
-            <textarea type="textarea" id="address" rows="4" />
-          </div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" className="mb-4" />
+          <label htmlFor="card">Credit Card</label>
+          <input type="text" id="card" className="mb-4" />
+          <label htmlFor="name">Shipping Address</label>
+          <textarea type="textarea" id="address" className="mb-4" rows="4" />
           <div className="d-flex justify-content-between">
             <div className="text-muted mb-4 pt-0 px-0 btn d-flex justify-content-start" id="catalog" onClick={this.handleClick}>
               Back to catalog
