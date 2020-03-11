9fb78ce09abb6ccb87f12281b4f50b636b18633c
Julian <jthrow00@gmail.com>
2020-03-11 13:45:36 -0700

    Added required prop to form input elements; modified handleChange regex to prevent multiple spaces and limit max chars

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -19,11 +19,12 @@ export default class CheckoutForm extends React.Component {
 
   handleChange(e) {
     const validationTests = {
-      name: /[^a-zA-Z ]/,
-      card: /[^\d ]/,
-      address: /[^a-zA-Z\d ]/
+      name: /^(?!.* {2,})[a-zA-Z ]{0,65}$/,
+      card: /^[\d]{0,16}$/,
+      address: /^(?!.* {2,})[a-zA-Z\d.,# ]{0,156}$/
     };
-    if (!validationTests[e.target.id].test(e.target.value)) this.setState({ [e.target.id]: e.target.value });
+
+    if (validationTests[e.target.id].test(e.target.value)) this.setState({ [e.target.id]: e.target.value });
   }
 
   handleSubmit(e) {
@@ -40,11 +41,11 @@ export default class CheckoutForm extends React.Component {
           <h5 className="d-flex align-items-center text-muted mb-4">Total Price: ${this.getTotalPrice()}</h5>
           <form className="d-flex flex-column">
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" className="mb-4" value={this.state.name} onChange={this.handleChange} />
+            <input type="text" id="name" className="mb-4" value={this.state.name} onChange={this.handleChange} required />
             <label htmlFor="card">Credit Card</label>
-            <input type="text" id="card" className="mb-4" value={this.state.card} onChange={this.handleChange} />
+            <input type="text" id="card" className="mb-4" value={this.state.card} onChange={this.handleChange} required />
             <label htmlFor="name">Shipping Address</label>
-            <textarea type="textarea" id="address" className="mb-4" value={this.state.address} rows="4" onChange={this.handleChange} />
+            <textarea type="textarea" id="address" className="mb-4" value={this.state.address} rows="4" onChange={this.handleChange} required />
             <div className="d-flex justify-content-between">
               <div className="text-muted mb-4 pt-0 px-0 btn d-flex justify-content-start" id="catalog" onClick={this.handleClick}>
                 Back to catalog
