cd6222d486a3c9030425b0d9637f85e9bb5d448c
Julian <jthrow00@gmail.com>
2020-03-10 23:15:26 -0700

    Moved back to catalog button and total price displays in CheckoutForm

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -5,10 +5,7 @@ export default class CheckoutForm extends React.Component {
     return (
       <div className="row mx-0">
         <div className="col-9 mx-auto d-flex flex-column">
-          <div className="text-muted mb-4 pt-0 px-0 btn d-flex justify-content-start"
-            onClick={() => this.props.setView('catalog', {})}>
-            Back to catalog
-          </div>
+          <h5 className="d-flex align-items-center text-muted">Total Price: $9999</h5>
           <h2 className="mb-4">My Cart</h2>
           <label htmlFor="name">Name</label>
           <input type="text" id="name" className="mb-4" />
@@ -17,7 +14,10 @@ export default class CheckoutForm extends React.Component {
           <label htmlFor="name">Shipping Address</label>
           <textarea type="textarea" id="address" className="mb-4" rows="4" />
           <div className="d-flex justify-content-between">
-            <h5 className="d-flex align-items-center text-muted">Total Price: $9999</h5>
+            <div className="text-muted mb-4 pt-0 px-0 btn d-flex justify-content-start"
+              onClick={() => this.props.setView('catalog', {})}>
+              Back to catalog
+            </div>
             <div>
               <button type="button" className="btn btn-primary">Checkout</button>
             </div>
