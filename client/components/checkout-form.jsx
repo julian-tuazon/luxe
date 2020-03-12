72f6d23af2b61e4c80b03cd76f99bc16e67bd349
Julian <jthrow00@gmail.com>
2020-03-12 03:32:06 -0700

    Changed error messages for name, address, card input fields

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -59,19 +59,19 @@ export default class CheckoutForm extends React.Component {
               <label htmlFor="name">Name</label>
               <input type="text" id="name" className={'form-control' + this.isValid('name')} value={this.state.name} onChange={this.handleChange} onBlur={this.handleBlur} minLength={5} maxLength={67} required />
               <div className="valid-feedback">Valid.</div>
-              <small className="invalid-feedback">Please fill out this field.</small>
+              <small className="invalid-feedback">Minimum of 5 characters required.</small>
             </div>
             <div className="form-group">
               <label htmlFor="card">Credit Card</label>
               <input type="text" id="card" className={'form-control' + this.isValid('card')} value={this.state.card} onChange={this.handleChange} onBlur={this.handleBlur} minLength={16} maxLength={16} required />
               <div className="valid-feedback">Valid.</div>
-              <small className="invalid-feedback">Please fill out this field.</small>
+              <small className="invalid-feedback">Please enter a valid 16 digit card number.</small>
             </div>
             <div className="form-group">
               <label htmlFor="name">Shipping Address</label>
               <textarea type="textarea" id="address" className={'form-control' + this.isValid('address')} value={this.state.address} rows="4" onChange={this.handleChange} onBlur={this.handleBlur} minLength={21} maxLength={156} required />
               <div className="valid-feedback">Valid.</div>
-              <small className="invalid-feedback">Please fill out this field.</small>
+              <small className="invalid-feedback">Minimum of 21 characters required.</small>
             </div>
             <div className="d-flex justify-content-between">
               <div>
