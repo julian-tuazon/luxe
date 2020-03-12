5e06fb01c05a8c9868958d6ffaa974d1d547be2c
Julian <jthrow00@gmail.com>
2020-03-12 03:57:40 -0700

    Added margin bottom to form group elements; absolute positioned form error messasges

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -55,23 +55,23 @@ export default class CheckoutForm extends React.Component {
           <h2 className="mb-4">My Cart</h2>
           <h5 className="d-flex align-items-center text-muted mb-4">Total Price: ${this.getTotalPrice()}</h5>
           <form className="d-flex flex-column needs-validation" noValidate onSubmit={this.handleSubmit}>
-            <div className="form-group">
+            <div className="form-group mb-5">
               <label htmlFor="name">Name</label>
               <input type="text" id="name" className={'form-control' + this.isValid('name')} value={this.state.name} onChange={this.handleChange} onBlur={this.handleBlur} minLength={5} maxLength={67} required />
               <div className="valid-feedback">Valid.</div>
-              <small className="invalid-feedback">Minimum of 5 characters required.</small>
+              <small className="invalid-feedback position-absolute">Minimum of 5 characters required.</small>
             </div>
-            <div className="form-group">
+            <div className="form-group mb-5">
               <label htmlFor="card">Credit Card</label>
               <input type="text" id="card" className={'form-control' + this.isValid('card')} value={this.state.card} onChange={this.handleChange} onBlur={this.handleBlur} minLength={16} maxLength={16} required />
               <div className="valid-feedback">Valid.</div>
-              <small className="invalid-feedback">Please enter a valid 16 digit card number.</small>
+              <small className="invalid-feedback position-absolute">Please enter a valid 16 digit card number.</small>
             </div>
-            <div className="form-group">
+            <div className="form-group mb-5">
               <label htmlFor="name">Shipping Address</label>
               <textarea type="textarea" id="address" className={'form-control' + this.isValid('address')} value={this.state.address} rows="4" onChange={this.handleChange} onBlur={this.handleBlur} minLength={21} maxLength={156} required />
               <div className="valid-feedback">Valid.</div>
-              <small className="invalid-feedback">Minimum of 21 characters required.</small>
+              <small className="invalid-feedback position-absolute">Minimum of 21 characters required.</small>
             </div>
             <div className="d-flex justify-content-between">
               <div>
