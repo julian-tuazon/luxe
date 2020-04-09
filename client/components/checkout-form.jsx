c938a7c70e995dbc1acfe532616bea34848c942b
Julian <jthrow00@gmail.com>
2020-04-08 18:31:24 -0700

    Removed unused valid feedback div in CheckoutForm render

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -76,19 +76,16 @@ export default class CheckoutForm extends React.Component {
             <div className="form-group mb-5">
               <label htmlFor="name">Name</label>
               <input type="text" id="name" className={this.setInputClassName('name')} value={this.state.name} onChange={this.handleChange} onBlur={this.handleBlur} minLength={5} maxLength={67} required />
-              {/* <div className="valid-feedback">Valid.</div> */}
               <small className="invalid-feedback position-absolute">Minimum of 5 characters required.</small>
             </div>
             <div className="form-group mb-5">
               <label htmlFor="card">Credit Card</label>
               <input type="text" id="card" className={this.setInputClassName('card')} value={this.state.card} onChange={this.handleChange} onBlur={this.handleBlur} minLength={16} maxLength={16} required />
-              {/* <div className="valid-feedback">Valid.</div> */}
               <small className="invalid-feedback position-absolute">Please enter a valid 16 digit card number.</small>
             </div>
             <div className="form-group mb-5">
               <label htmlFor="name">Shipping Address</label>
               <textarea type="textarea" id="address" className={this.setInputClassName('address')} value={this.state.address} rows="4" onChange={this.handleChange} onBlur={this.handleBlur} minLength={21} maxLength={156} required />
-              {/* <div className="valid-feedback">Valid.</div> */}
               <small className="invalid-feedback position-absolute">Minimum of 21 characters required.</small>
             </div>
             <div className="d-flex justify-content-between">
