b1fc0da4ec73d13ad2de43ee80e76739cbcf7f9f
Julian <jthrow00@gmail.com>
2020-04-08 22:06:26 -0700

    Removed inline styling for ProductListItem img; moved object-fit declaration for img to css stylesheet and removed hard coded height

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -4,10 +4,7 @@ export default function ProductListItem(props) {
   return (
     <div className="col mb-4">
       <div className="card h-100 btn" id={props.product.productId} onClick={props.handleClick}>
-        <img src={props.product.image}
-          className="card-img-top"
-          alt={props.product.name}
-          style={{ objectFit: 'contain', height: '50%' }} />
+        <img src={props.product.image} className="card-img-top" alt={props.product.name} />
         <div className="card-body">
           <h5 className="card-title">{props.product.name}</h5>
           <p>${props.product.price}</p>
