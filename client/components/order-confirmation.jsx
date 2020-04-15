6da11648493a00f74a43a20f033aafbf1751fe24
Julian <jthrow00@gmail.com>
2020-04-14 21:46:38 -0700

    Created OrderConfirmation component that is shown after placing order and thanks user

---
+++ client/components/order-confirmation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function OrderConfirmation(props) {
+  return (
+    <div className="container text-center col-11">
+      <h1 className="mb-5">Order Confirmation</h1>
+      <h5 className="mb-4">Thank you for using this demo.</h5>
+      <h5 className="mb-5">No real purchases have been made and no payment processing will occur.</h5>
+      <div>
+        <button type="button" id="catalog" className="btn btn-outline-info" onClick={() => props.setView('catalog', {})}>Back to Catalog</button>
+      </div>
+    </div>
+  );
+}
