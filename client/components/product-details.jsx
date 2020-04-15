1e71f60f9d72e69b8d60744a3b890aa07d4ce152
Julian <jthrow00@gmail.com>
2020-04-14 20:44:59 -0700

    Modified ProductDetails to show modal after item is added to cart; added renderModal method and state property of showModal

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { product: null };
+    this.state = { product: null, showModal: false };
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -19,13 +19,54 @@ export default class ProductDetails extends React.Component {
   }
 
   handleClick(e) {
-    e.target.id === 'catalog' ? this.props.setView('catalog', {}) : this.props.addToCart(this.state.product);
+    if (e.target.id === 'catalog') return this.props.setView('catalog', {});
+    if (e.target.id === 'addToCart') return this.addToCart();
+  }
+
+  addToCart() {
+    this.props.addToCart(this.state.product);
+    this.setState({ showModal: true });
+  }
+
+  renderModal() {
+    const modalClassName = this.state.showModal ? 'modal overlay d-block' : 'modal overlay';
+    const hideModal = e => {
+      if (e.target.id === 'shopping') return this.props.setView('catalog', {});
+      if (e.target.id === 'cart') return this.props.setView('cart', {});
+      this.setState({ showModal: false });
+    };
+
+    return (
+      <div className={modalClassName} tabIndex="-1" role="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Item Added to Cart</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={hideModal}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <p>
+                <i className="far fa-check-circle text-success mr-2"></i>
+                {this.state.product.name} has been added to your cart.
+              </p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" id="shopping" className="btn btn-primary" onClick={hideModal}>Continue Shopping</button>
+              <button type="button" id="cart" className="btn btn-secondary" onClick={hideModal} data-dismiss="modal">View Cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   render() {
     if (!this.state.product) return null;
     return (
       <div className="container mb-5">
+        {this.renderModal()}
         <div className="card card-body">
           <div className="mb-3">
             <button type="button" id="catalog" className="btn btn-outline-info" onClick={this.handleClick}>Back to Catalog</button>
