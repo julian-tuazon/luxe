cb679911a8985301a8bcbf9b4a730e6effd37df4
Julian <jthrow00@gmail.com>
2020-04-15 07:17:51 -0700

    Changed sticky-top to fixed-top to prevent header from disappearing after scrolling down far enough

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ export default function Header(props) {
   const cartItemCount = props.cart.reduce((acc, cur) => acc + cur.quantity, 0);
 
   return (
-    <div className="py-3 mx-0 mb-5 row text-white bg-dark sticky-top">
+    <div className="py-3 mx-0 mb-5 row text-white bg-dark fixed-top">
       <div className="col-11 mx-auto d-flex justify-content-between">
         <div className="btn text-white" onClick={() => props.setView('catalog', {})}>
           <i className="fas fa-glass-martini-alt mr-3"></i>
