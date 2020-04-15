77663ccd13980ee5ddd1e0bf8be2213838555945
Julian <jthrow00@gmail.com>
2020-04-15 16:45:34 -0700

    Added functionality to Luxe logo in footer to scroll to top upon click

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 export default function Footer(props) {
+  const scrollToTop = () => window.scrollTo(0, 0);
+
   return (
     <div className="py-4 mx-0 row text-white bg-dark footer">
       <div className="col-11 mx-auto d-flex flex-column flex-sm-row justify-content-between align-items-center">
         <div className="mb-4 mb-sm-0">Live Luxuriously &trade;</div>
-        <div className="d-flex align-items-center mb-4 mb-sm-0">
+        <div className="d-flex align-items-center btn text-white mb-4 mb-sm-0" onClick={scrollToTop}>
           <i className="fas fa-glass-martini-alt fa-lg mr-3"></i>
           <h5>L U X E</h5>
         </div>
