0b7ee0921ea626d5457addcbc5591d96a8f6f649
Julian <jthrow00@gmail.com>
2020-04-15 16:35:54 -0700

    Imported QuantityInput component and rendered in CartSummaryItem render

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,51 +1,52 @@
 import React from 'react';
+import QuantityInput from './quantity-input';
 
 export default class CartSummaryItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { showModal: false, quantity: this.props.item.quantity };
-    this.updateQuantity = this.updateQuantity.bind(this);
+    this.state = { showModal: false };
+    // this.updateQuantity = this.updateQuantity.bind(this);
     this.removeCartItem = this.removeCartItem.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleBlur = this.handleBlur.bind(this);
   }
 
-  handleChange(e) {
-    const input = e.target;
-    const validNum = /(?!^0)(^[\d]*$)/;
-    if (validNum.test(input.value)) this.setState({ quantity: input.value });
-  }
+  // handleChange(e) {
+  //   const input = e.target;
+  //   const validNum = /(?!^0)(^[\d]*$)/;
+  //   if (validNum.test(input.value)) this.setState({ quantity: input.value });
+  // }
 
-  handleBlur(e) {
-    if (!this.state.quantity) {
-      return this.setState({
-        quantity: 1
-      }, () => this.props.updateQuantity(this.props.item.productId, Number(this.state.quantity)));
-    }
-    this.props.updateQuantity(this.props.item.productId, Number(this.state.quantity));
-  }
+  // handleBlur(e) {
+  //   if (!this.state.quantity) {
+  //     return this.setState({
+  //       quantity: 1
+  //     }, () => this.props.updateQuantity(this.props.item.productId, Number(this.state.quantity)));
+  //   }
+  //   this.props.updateQuantity(this.props.item.productId, Number(this.state.quantity));
+  // }
 
-  updateQuantity(e) {
-    if (!this.props.canClick) return;
-    if (e.currentTarget.id === 'plus' && this.props.item.quantity < 99) {
-      return this.setState({
-        quantity: Number(this.state.quantity) + 1
-      }, () => this.props.updateQuantity(this.props.item.productId, this.state.quantity));
-    }
-    if (e.currentTarget.id === 'minus' && this.props.item.quantity > 1) {
-      return this.setState({
-        quantity: Number(this.state.quantity) - 1
-      }, () => this.props.updateQuantity(this.props.item.productId, this.state.quantity));
-    }
-  }
+  // updateQuantity(e) {
+  //   if (!this.props.canClick) return;
+  //   if (e.currentTarget.id === 'plus' && this.props.item.quantity < 99) {
+  //     return this.setState({
+  //       quantity: Number(this.state.quantity) + 1
+  //     }, () => this.props.updateQuantity(this.props.item.productId, this.state.quantity));
+  //   }
+  //   if (e.currentTarget.id === 'minus' && this.props.item.quantity > 1) {
+  //     return this.setState({
+  //       quantity: Number(this.state.quantity) - 1
+  //     }, () => this.props.updateQuantity(this.props.item.productId, this.state.quantity));
+  //   }
+  // }
 
-  setMinusButtonStatus() {
-    return (this.props.item.quantity > 1) ? 'btn ml-2 text-primary' : 'btn ml-2 disabled';
-  }
+  // setMinusButtonStatus() {
+  //   return (this.props.item.quantity > 1) ? 'btn ml-2 text-primary' : 'btn ml-2 disabled';
+  // }
 
-  setPlusButtonStatus() {
-    return (this.props.item.quantity < 99) ? 'btn text-primary' : 'btn disabled';
-  }
+  // setPlusButtonStatus() {
+  //   return (this.props.item.quantity < 99) ? 'btn text-primary' : 'btn disabled';
+  // }
 
   removeCartItem() {
     this.setState({ showModal: true });
@@ -98,7 +99,8 @@ export default class CartSummaryItem extends React.Component {
             <p className="d-inline-block text-success border border-success rounded p-2">${this.props.item.price}</p>
           </div>
           <p>{this.props.item.shortDescription}</p>
-          <div className="form-group d-flex align-items-center mb-3">
+          <QuantityInput item={this.props.item} updateQuantity={this.props.updateQuantity} canClick={this.props.canClick} />
+          {/* <div className="form-group d-flex align-items-center mb-3">
             <label htmlFor="quantity" className="mb-0">Quantity:</label>
             <button type="button" id="minus" className={this.setMinusButtonStatus()} onClick={this.updateQuantity}>
               <i className="fas fa-minus fa-xs"></i>
@@ -107,7 +109,7 @@ export default class CartSummaryItem extends React.Component {
             <button type="button" id="plus" className={this.setPlusButtonStatus()} onClick={this.updateQuantity}>
               <i className="fas fa-plus fa-xs"></i>
             </button>
-          </div>
+          </div> */}
           <div>
             <button type="button" id="delete" className="btn text-secondary px-0" onClick={this.removeCartItem}>
               <i className="fas fa-times text-secondary fa-sm mr-2"></i>
