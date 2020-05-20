6a6a757550eff18280f974d54d9a118c29894010
Julian <jthrow00@gmail.com>
2020-05-20 02:14:12 -0700

    Deleted tests folder

--- server/validation-tests.js
+++ server/validation-tests.js
@@ -3,11 +3,11 @@ function isValidNum(num) {
 }
 
 function isValidName(name) {
-  return name && /^(?!.* {2,})(?=\S)(?=.*\S$)[a-zA-Z ]{5,67}$/.test(name);
+  return !!name && /^(?!.* {2,})(?=\S)(?=.*\S$)[a-zA-Z ]{5,67}$/.test(name);
 }
 
 function isValidAddressOne(addressOne) {
-  return addressOne && /^(?!.* {2,})(?=\S)(?=.*\S$)[a-zA-Z\d.,# ]{6,42}$/.test(addressOne);
+  return !!addressOne && /^(?!.* {2,})(?=\S)(?=.*\S$)[a-zA-Z\d.,# ]{6,42}$/.test(addressOne);
 }
 
 function isValidAddressTwo(addressTwo) {
@@ -16,31 +16,31 @@ function isValidAddressTwo(addressTwo) {
 }
 
 function isValidCity(city) {
-  return city && /^(?!.* {2,})(?=\S)(?=.*\S$)[a-zA-Z.\- ]{3,50}$/.test(city);
+  return !!city && /^(?!.* {2,})(?=\S)(?=.*\S$)[a-zA-Z.\- ]{3,50}$/.test(city);
 }
 
 function isValidState(state) {
-  return state && /^[a-zA-Z]{2}$/.test(state);
+  return /^[a-zA-Z]{2}$/.test(state);
 }
 
 function isValidZipCode(zipCode) {
-  return zipCode && /^[\d]{5}$/.test(zipCode);
+  return /^[\d]{5}$/.test(zipCode);
 }
 
 function isValidCardNumber(cardNumber) {
-  return cardNumber && /^[\d]{16}$/.test(cardNumber);
+  return /^[\d]{16}$/.test(cardNumber);
 }
 
 function isValidCardMonth(cardMonth) {
-  return cardMonth && /^[\d]{2}$/.test(cardMonth);
+  return /^[\d]{2}$/.test(cardMonth);
 }
 
 function isValidCardYear(cardYear) {
-  return cardYear && /^[\d]{4}$/.test(cardYear);
+  return /^[\d]{4}$/.test(cardYear);
 }
 
 function isValidCardCVV(cardCVV) {
-  return cardCVV && /^[\d]{3,4}$/.test(cardCVV);
+  return /^[\d]{3,4}$/.test(cardCVV);
 }
 
 exports.isValidNum = isValidNum;
