2f256cf838ed8541076c78edd2888344035154b7
Julian <jthrow00@gmail.com>
2020-05-22 04:49:12 -0700

    Modified session-middleware module to not store sessions in folder if NODE_ENV is test

--- server/session-middleware.js
+++ server/session-middleware.js
@@ -4,17 +4,21 @@ const sessionFileStore = require('session-file-store');
 
 const FileStore = sessionFileStore(expressSession);
 
+let store = new FileStore({
+  retries: 0,
+  ttl: 28800,
+  path: path.join(__dirname, 'sessions/')
+});
+
+if (process.env.NODE_ENV === 'test') store = null;
+
 const sessionMiddleware = expressSession({
   cookie: {
     sameSite: true
   },
   resave: false,
   rolling: true,
-  store: new FileStore({
-    retries: 0,
-    ttl: 28800,
-    path: path.join(__dirname, 'sessions/')
-  }),
+  store,
   saveUninitialized: false,
   secret: process.env.SESSION_SECRET
 });
